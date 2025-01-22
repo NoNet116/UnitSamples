3f1535bfe983727d39d621136bf612a1db65d7e3
NoNet <NoNet@TEMP>
2025-01-22 21:26:52 +0300

    Added a unit test for addition and multiplication

--- Calculator.Tests/calculatorTest.cs
+++ Calculator.Tests/calculatorTest.cs
@@ -9,22 +9,36 @@ namespace Calculator.Tests
         [Test]
         public void Division_MustReturnCorrectValue()
         {
-            UnitSamples.Calculator calculator = new UnitSamples.Calculator();
+            var calculator = new UnitSamples.Calculator();
             Assert.That(calculator.Division(200, 10) == 20);
-        }
-
-        [Test]
-        public void Subtraction_MustReturnCorrectValue()
-        {
-            UnitSamples.Calculator calculator = new UnitSamples.Calculator();
-            Assert.That(calculator.Subtraction(300, 10) == 290);
         }
 
         [Test]
         public void Division_MustThrowException()
         {
-            UnitSamples.Calculator calculator = new UnitSamples.Calculator();
+            var calculator = new UnitSamples.Calculator();
             Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new UnitSamples.Calculator();
+            Assert.That(calculator.Subtraction(15, 19) == -4);
+        }
+
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new UnitSamples.Calculator();
+            Assert.That(calculator.Additional(15, 19) == 34);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new UnitSamples.Calculator();
+            Assert.That(calculator.Miltiplication(15, 19) == 285);
         }
 
     }
